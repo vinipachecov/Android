8f3160265b48109ada1839d5bb4992dc34af3525
vinipachecov <vinipachecov@gmail.com>
2017-04-15 21:59:56 -0300

    Animations

---
+++ Udemy/The Complete Android N Developer Course/Animations/app/src/main/java/com/example/root/animations/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.root.animations;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view){
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+        ImageView homer = (ImageView) findViewById(R.id.homer);
+
+        bart.animate().alpha(0f).setDuration(2000);
+        homer.animate().alpha(1f).setDuration(2000);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
