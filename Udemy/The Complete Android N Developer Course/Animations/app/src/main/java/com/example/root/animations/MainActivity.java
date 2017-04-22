d2f7ae7783aab5d910f6ddea329e894ebaadebd8
vinipachecov <vinipachecov@gmail.com>
2017-04-22 00:36:17 -0300

    fix

--- Udemy/The Complete Android N Developer Course/Animations/app/src/main/java/com/example/root/animations/MainActivity.java
+++ Udemy/The Complete Android N Developer Course/Animations/app/src/main/java/com/example/root/animations/MainActivity.java
@@ -9,11 +9,31 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     public void fade(View view){
+
         ImageView bart = (ImageView) findViewById(R.id.bart);
+//
+//        bart.animate().translationXBy(-1000f).setDuration(2000);
+
+        bart.animate().translationXBy(1000f).translationYBy(1000f).rotation(3600).setDuration(2000);
+
         ImageView homer = (ImageView) findViewById(R.id.homer);
 
-        bart.animate().alpha(0f).setDuration(2000);
-        homer.animate().alpha(1f).setDuration(2000);
+       // bart.animate().translationXBy(1000f).setDuration(2000);
+
+        //bart.animate().alpha(0f).setDuration(2000);
+
+
+
+        //homer.animate().alpha(1f).setDuration(2000);
+
+        //ROTATION BART
+
+        //bart.animate().rotation(1800f).setDuration(2000);
+
+        //SHRINK - half the size
+
+        //bart.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+
     }
 
 
@@ -21,5 +41,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //challenge
+
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+
+        bart.setTranslationX(-1000f);
+        bart.setTranslationY(-1000f);
+
+        /*
+
+
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+
+        bart.setTranslationX(-1000f);
+        */
     }
 }
---
+++ Udemy/The Complete Android N Developer Course/Connect3/app/src/main/java/com/example/root/connect3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.root.connect3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1000f).setDuration(300);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
