e41ab3cad76e76e1ee817a7b231009af11f97c7d
vinicius p vieira <vinipachecov@gmail.com>
2017-10-03 11:07:52 -0300

    Initial commit from Android Studio

---
+++ Udemy/The Complete Android N Developer Course/VersionControlDemo/app/src/main/java/com/example/vinicius/versioncontroldemo/MainActivity.java
@@ -0,0 +1,16 @@
+ package com.example.vinicius.versioncontroldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "My first IDE version control app", Toast.LENGTH_SHORT).show();
+    }
+}
